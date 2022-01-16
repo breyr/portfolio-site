091e53aa28764a97e8bb3ef0423eec857c2e50ca
Brey Rivera <breyr2021@gmail.com>
2022-01-16 00:12:07 -0500

    fixed side nav bug

--- index.js
+++ index.js
@@ -4,17 +4,11 @@ navButton.addEventListener("click", openNav);
 /* Open the sidenav */
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
-  navButton.style.display = "none";
-}
-
-function showNavButton() {
-  navButton.style.display = "block";
 }
 
 /* Close/hide the sidenav */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  setTimeout(showNavButton, 400);
 }
 
 let sidenavListItems = [...document.querySelectorAll("nav > li > a")]
